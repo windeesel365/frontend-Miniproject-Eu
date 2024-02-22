5d19fa8ee5bd7e4b36db312971fb686b3801fab7
Palathip <windeesel365@gmail.com>
2024-02-22 21:55:47 +0700

    feat:Add javascript menuIcon for nav

---
+++ script.js
@@ -0,0 +1,17 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.querySelector('.menu-icon');
+    const menu = document.querySelector('.menu');
+
+    menuIcon.addEventListener('click', function() {
+        menu.classList.toggle('active');
+
+        var contactButton = document.querySelector('.contact-now');
+        if (contactButton.style.display === 'none' || contactButton.style.display === '') {
+            contactButton.style.display = 'block';
+        } else {
+            contactButton.style.display = 'none';
+        }
+        
+    });
+});
